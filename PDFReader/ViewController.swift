9b2818a5c2702ac815d09379b1fadb8bae21871b
maikytech <maikytech1307@gmail.com>
2020-03-31 21:40:02 -0500

    Start Project

---
+++ PDFReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PDFReader
+//
+//  Created by Maiqui Cedeño on 31/03/20.
+//  Copyright © 2020 PosetoStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
