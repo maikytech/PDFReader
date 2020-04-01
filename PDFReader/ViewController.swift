893522010ed5b86ee1f23086a1364e950faade78
maikytech <maikytech1307@gmail.com>
2020-04-01 12:29:54 -0500

    TableView Configuration

--- PDFReader/ViewController.swift
+++ PDFReader/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+//UITableViewDelegate, UITableViewDataSource are protocols to manage tables.
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+   var cellContents = ["Maiky", "Jessica", "Gio", "Poseto Studio", "Interstellar War", "Anun Chronics"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+       return cellContents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = cellContents[indexPath.row]
+        
+        return cell
+    }
 }
 
--- build/PDFReader.build/Debug-iphonesimulator/PDFReader.build/Objects-normal/x86_64/PDFReader-Swift.h
+++ build/PDFReader.build/Debug-iphonesimulator/PDFReader.build/Objects-normal/x86_64/PDFReader-Swift.h
@@ -181,6 +181,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #if __has_warning("-Watimport-in-framework-header")
 #pragma clang diagnostic ignored "-Watimport-in-framework-header"
 #endif
+@import Foundation;
 @import UIKit;
 #endif
 
@@ -227,12 +228,16 @@ SWIFT_CLASS("_TtC9PDFReader13SceneDelegate")
 - (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
 @end
 
+@class UITableView;
+@class UITableViewCell;
 @class NSBundle;
 @class NSCoder;
 
 SWIFT_CLASS("_TtC9PDFReader14ViewController")
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
 - (void)viewDidLoad;
+- (NSInteger)tableView:(UITableView * _Nonnull)tableView numberOfRowsInSection:(NSInteger)section SWIFT_WARN_UNUSED_RESULT;
+- (UITableViewCell * _Nonnull)tableView:(UITableView * _Nonnull)tableView cellForRowAtIndexPath:(NSIndexPath * _Nonnull)indexPath SWIFT_WARN_UNUSED_RESULT;
 - (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
 - (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
 @end
